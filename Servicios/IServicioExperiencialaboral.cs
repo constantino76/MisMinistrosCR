414f5154e947bccd8ee2b8b6082b56f5abfdf591
constantino76 <ronald.retana@uned.cr>
2024-03-31 20:54:56 -0600

    Agregando servicios a empleados y experiencia laboral

--- Servicios/IServicioExperiencialaboral.cs
+++ Servicios/IServicioExperiencialaboral.cs
@@ -1,6 +1,11 @@
-﻿namespace MisministrosCR_VERSION1.Servicios
+﻿using MisministrosCR_VERSION1.Models;
+
+namespace MisministrosCR_VERSION1.Servicios
 {
     public interface IServicioExperiencialaboral
     {
+        public Task<bool> Insertar(Experiencia_trabajo exptrabajo);
+        public Task<Oferente> Buscar(String  id);
+        public Task<Oferente> Actualizar();
     }
 }
--- Servicios/ServicioExperienciaLaboral.cs
+++ Servicios/ServicioExperienciaLaboral.cs
@@ -1,6 +1,22 @@
-﻿namespace MisministrosCR_VERSION1.Servicios
+﻿using MisministrosCR_VERSION1.Models;
+
+namespace MisministrosCR_VERSION1.Servicios
 {
-    public class ServicioExperienciaLaboral
+    public class ServicioExperienciaLaboral : IServicioExperiencialaboral
     {
+        public Task<Oferente> Actualizar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Oferente> Buscar(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insertar(Experiencia_trabajo exptrabajo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
